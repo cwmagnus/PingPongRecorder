eaff8f6beaa12c12fcab0aea0e24836609cd5bbe
Christopher Magnuson <cwmagnus0@gmail.com>
2019-04-23 16:22:24 -0400

    Initial Commit

---
+++ PingPongRecorder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PingPongRecorder
+//
+//  Created by Chris Magnuson on 4/23/19.
+//  Copyright © 2019 Chris Magnuson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
