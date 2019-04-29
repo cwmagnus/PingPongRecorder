c64ebe8d31fa2cc1fa8159c3faf04bdeabdcf8cf
Christopher Magnuson <cwmagnus0@gmail.com>
2019-04-29 15:38:40 -0400

    Added scroll view to login and register scenes

--- PingPongRecorder/Controllers/RegisterVC.swift
+++ PingPongRecorder/Controllers/RegisterVC.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class RegisterVC : UIViewController {
     
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        performSegue(withIdentifier: "LoginSuccess", sender: self)
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        performSegue(withIdentifier: "Login", sender: self)
     }
 }
